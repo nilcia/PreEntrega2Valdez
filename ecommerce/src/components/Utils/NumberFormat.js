02f444ad6483319da629cb9a2ea8518f3df8d6c6
Vanessa Valdez <nilcia.valdezgonzalez@claro.com.py>
2023-01-13 20:09:46 -0300

    Correcion de formateo de numeros

--- ecommerce/src/components/Utils/NumberFormat.js
+++ ecommerce/src/components/Utils/NumberFormat.js
@@ -1,5 +1,5 @@
 const NumberFormat = ({number}) => {
-    let numberFormatter = new Intl.NumberFormat('de-DE', {  maximumSignificantDigits: 1 }).format(number);
+    let numberFormatter = new Intl.NumberFormat('de-DE', {  maximumSignificantDigits: 2 }).format(number);
     return 'Gs. ' + numberFormatter;
 }
 export default NumberFormat;
